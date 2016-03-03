e5f72aa90f63b9442a53ac4def4fd383e6baf2b6
patrickstark <patrick.villanueva@easyshop.ph>
2016-03-03 10:23:04 +0800

    Added new key for runningTotal in Cash Management module.

--- ExpressInternal/app/src/main/java/com/yilinker/expressinternal/model/CashHistory.java
+++ ExpressInternal/app/src/main/java/com/yilinker/expressinternal/model/CashHistory.java
@@ -22,6 +22,7 @@ public class CashHistory implements Parcelable {
     private Date date;
     private String type;
     private String waybillNo;
+    private double runningTotal;
 
     public CashHistory(com.yilinker.core.model.express.internal.CashHistory object){
 
@@ -31,31 +32,19 @@ public class CashHistory implements Parcelable {
         this.date = DateUtility.convertStringToDate(object.getDate(), SERVER_DATE_FORMAT);
         this.type = object.getType();
         this.waybillNo = object.getWaybillNo();
-
+        this.runningTotal = object.getRunningTotal();
         //temp
 //        this.jobOrderNo = String.valueOf(object.getJobOrderNo());
 
     }
 
-    protected CashHistory(Parcel in) {
-        action = in.readString();
-        jobOrderNo = in.readString();
-        amount = in.readDouble();
-        type = in.readString();
-        waybillNo = in.readString();
+    public double getRunningTotal() {
+        return runningTotal;
     }
 
-    public static final Creator<CashHistory> CREATOR = new Creator<CashHistory>() {
-        @Override
-        public CashHistory createFromParcel(Parcel in) {
-            return new CashHistory(in);
-        }
-
-        @Override
-        public CashHistory[] newArray(int size) {
-            return new CashHistory[size];
-        }
-    };
+    public void setRunningTotal(double runningTotal) {
+        this.runningTotal = runningTotal;
+    }
 
     public String getAction() {
         return action;
@@ -105,6 +94,7 @@ public class CashHistory implements Parcelable {
         this.waybillNo = waybillNo;
     }
 
+
     @Override
     public int describeContents() {
         return 0;
@@ -112,13 +102,33 @@ public class CashHistory implements Parcelable {
 
     @Override
     public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(action);
-        dest.writeString(jobOrderNo);
-        dest.writeDouble(amount);
-        dest.writeString(type);
-        dest.writeString(waybillNo);
+        dest.writeString(this.action);
+        dest.writeString(this.jobOrderNo);
+        dest.writeDouble(this.amount);
+        dest.writeLong(date != null ? date.getTime() : -1);
+        dest.writeString(this.type);
+        dest.writeString(this.waybillNo);
+        dest.writeDouble(this.runningTotal);
     }
 
+    protected CashHistory(Parcel in) {
+        this.action = in.readString();
+        this.jobOrderNo = in.readString();
+        this.amount = in.readDouble();
+        long tmpDate = in.readLong();
+        this.date = tmpDate == -1 ? null : new Date(tmpDate);
+        this.type = in.readString();
+        this.waybillNo = in.readString();
+        this.runningTotal = in.readDouble();
+    }
 
+    public static final Creator<CashHistory> CREATOR = new Creator<CashHistory>() {
+        public CashHistory createFromParcel(Parcel source) {
+            return new CashHistory(source);
+        }
 
+        public CashHistory[] newArray(int size) {
+            return new CashHistory[size];
+        }
+    };
 }
