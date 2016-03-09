60fe1104554b65805399cac34d986bafe70afe8b
patrickstark <patrick.villanueva@easyshop.ph>
2016-03-09 15:49:21 +0800

    Moved files to respective packages.

--- ExpressInternal/app/src/main/java/com/yilinker/expressinternal/mvp/presenter/login/IRegistrationSignUpPresenter.java
+++
@@ -1,9 +0,0 @@
-package com.yilinker.expressinternal.mvp.presenter.login;
-
-/**
- * Created by Patrick on 3/8/2016.
- */
-public interface IRegistrationSignUpPresenter {
-
-    public void validateInput(String mobileNumber);
-}
