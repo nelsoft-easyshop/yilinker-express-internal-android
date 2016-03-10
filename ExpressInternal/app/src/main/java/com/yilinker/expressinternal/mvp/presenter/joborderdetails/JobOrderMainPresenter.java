3c0db263a7a5133e97d1d6071e6dfe116b11528e
rlcoronadoesmobile <rhys.coronado@easyshop.ph>
2016-03-10 10:32:30 +0800

    Added button handlers for Delivery, Drop Off, improved layout, added loader.

---
+++ ExpressInternal/app/src/main/java/com/yilinker/expressinternal/mvp/presenter/joborderdetails/JobOrderMainPresenter.java
@@ -0,0 +1,16 @@
+package com.yilinker.expressinternal.mvp.presenter.joborderdetails;
+
+import com.yilinker.expressinternal.model.JobOrder;
+import com.yilinker.expressinternal.mvp.presenter.BasePresenter;
+import com.yilinker.expressinternal.mvp.view.joborderdetails.IJobOrderMainView;
+
+/**
+ * Created by rlcoronado on 09/03/2016.
+ */
+public class JobOrderMainPresenter extends BasePresenter<JobOrder, IJobOrderMainView>{
+
+    @Override
+    protected void updateView() {
+
+    }
+}
